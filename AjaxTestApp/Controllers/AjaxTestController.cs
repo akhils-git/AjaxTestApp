adcb9c45274013eb4130a48230b7323b7119a3ca
Akhil mon <akhilmon@hotmail.com>
2021-05-20 09:57:34 +0530

    Add project files.

---
+++ AjaxTestApp/Controllers/AjaxTestController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AjaxTestApp.Controllers
+{
+    public class AjaxTestController : Controller
+    {
+        // GET: AjaxTest
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult FirstAjax()
+        {
+            return Json("Sachin", JsonRequestBehavior.AllowGet);
+        }
+
+    }
+}
